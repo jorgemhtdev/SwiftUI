23cb18b5c1452de41cd47b6cb201273d61e20ecf
jorgemhtdev <hola@jorgemht.dev>
2023-02-05 17:10:28 +0100

    First build and commit JMaps

---
+++ JMaps/JMaps/JMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JMapsApp.swift
+//  JMaps
+//
+//  Created by jorgemht on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct JMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
